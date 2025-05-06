09d2e8fa87e34cfe8048ed712a02b9fa4cac9ee3
K0taka <144228630+K0taka@users.noreply.github.com>
2025-05-07 04:51:44 +0500

    Kind model in Core

--- Teatronik.Core/Models/Kind.cs
+++ Teatronik.Core/Models/Kind.cs
@@ -1,7 +1,33 @@
-﻿namespace Teatronik.Core.Models
+﻿using Teatronik.Core.Common;
+
+namespace Teatronik.Core.Models
 {
     public class Kind
     {
+        public Guid Id { get; }
+        public string KindName { get; private set; }
 
+        private Kind(Guid id, string kindName)
+        {
+            Id = id;
+            KindName = kindName;
+        }
+
+        public Kind(string kindName) : this(Guid.NewGuid(), kindName) {}
+
+        public static Result<Kind> Create(string kindName)
+        {
+            if (string.IsNullOrWhiteSpace(kindName))
+                return Result<Kind>.Fail("Kind name must be not empty");
+            return Result<Kind>.Ok(new(kindName));
+        }
+
+        public Result UpdateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return Result.Fail("Kind name must be not empty");
+            KindName = name;
+            return Result.Ok();
+        }
     }
 }
