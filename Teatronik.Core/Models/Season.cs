d1a4b70d8f1d5e653592c107b435ba7adb043cea
K0taka <144228630+K0taka@users.noreply.github.com>
2025-05-07 05:52:24 +0500

    Season model in Core

--- Teatronik.Core/Models/Season.cs
+++ Teatronik.Core/Models/Season.cs
@@ -1,7 +1,33 @@
-﻿namespace Teatronik.Core.Models
+﻿using Teatronik.Core.Common;
+
+namespace Teatronik.Core.Models
 {
     public class Season
     {
+        public Guid Id { get; }
+        public string SeasonName { get; private set; }
 
+        private Season(Guid id, string seasonName)
+        {
+            Id = id;
+            SeasonName = seasonName;
+        }
+
+        public Season(string seasonName) : this(Guid.NewGuid(), seasonName) { }
+
+        public static Result<Season> Create(string seasonName)
+        {
+            if (string.IsNullOrWhiteSpace(seasonName))
+                return Result<Season>.Fail("Season name must be not empty");
+            return Result<Season>.Ok(new(seasonName));
+        }
+
+        public Result UpdateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return Result.Fail("Season name must be not empty");
+            SeasonName = name;
+            return Result.Ok();
+        }
     }
 }
