5d070d6b1dc7377555ab63b11c8773f2fb7fa42f
K0taka <144228630+K0taka@users.noreply.github.com>
2025-05-07 05:43:47 +0500

    Prop schema model in core

--- Teatronik.Core/Models/PropSchema.cs
+++ Teatronik.Core/Models/PropSchema.cs
@@ -1,7 +1,75 @@
-﻿namespace Teatronik.Core.Models
+﻿using Teatronik.Core.Common;
+
+namespace Teatronik.Core.Models
 {
     public class PropSchema
     {
+        public Guid Id { get; }
+        public string SchemaName { get; private set; }
+        public float Length { get; private set; }
+        public float Width { get; private set; }
+        public float Height { get; private set; }
 
+        private PropSchema(Guid id, string schemaName, float length, float width, float height)
+        {
+            Id = id;
+            SchemaName = schemaName;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        private PropSchema(string schemaName, float length, float width, float height) :
+            this(Guid.NewGuid(), schemaName, length, width, height)
+        { }
+
+        public static Result<PropSchema> Create(string schemaName, float length, float width, float height)
+        {
+            if (string.IsNullOrWhiteSpace(schemaName))
+                return Result<PropSchema>.Fail("Schema name must be not empty");
+
+            if (length <= 0)
+                return Result<PropSchema>.Fail("Length must be positive");
+
+            if (width <= 0)
+                return Result<PropSchema>.Fail("Width must be positive");
+
+            if (height <= 0)
+                return Result<PropSchema>.Fail("Height must be positive");
+
+            return Result<PropSchema>.Ok(new(schemaName, length, width, height));
+        }
+
+        public Result UpdateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return Result.Fail("Schema name must be not empty");
+            SchemaName = name;
+            return Result.Ok();
+        }
+
+        public Result UpdateLength(float length)
+        {
+            if (length <= 0)
+                return Result<PropSchema>.Fail("Length must be positive");
+            Length = length;
+            return Result.Ok();
+        }
+
+        public Result UpdateWidth(float width)
+        {
+            if (width <= 0)
+                return Result<PropSchema>.Fail("Width must be positive");
+            Width = width;
+            return Result.Ok();
+        }
+
+        public Result UpdateHeight(float height)
+        {
+            if (height <= 0)
+                return Result<PropSchema>.Fail("Height must be positive");
+            Height = height;
+            return Result.Ok();
+        }
     }
 }
