afa0870100e7b73f50ad41d67bee75aea01624ac
K0taka <144228630+K0taka@users.noreply.github.com>
2025-05-07 05:28:46 +0500

    Prop model in Core

--- Teatronik.Core/Models/Prop.cs
+++ Teatronik.Core/Models/Prop.cs
@@ -1,7 +1,42 @@
-﻿namespace Teatronik.Core.Models
+﻿using Teatronik.Core.Common;
+
+namespace Teatronik.Core.Models
 {
     public class Prop
     {
+        public Guid Id { get; }
+        public string PropName { get; private set; }
+        public DateOnly Created { get; }
+        public Guid SchemaId { get; }
 
+        private Prop(Guid id, string propName, DateOnly created, Guid schemaId)
+        {
+            Id = id;
+            PropName = propName;
+            Created = created;
+            SchemaId = schemaId;
+        }
+
+        private Prop(string propName, DateOnly created, Guid schemaId) : this(Guid.NewGuid(), propName, created, schemaId) {}
+
+        public static Result<Prop> Create(string propName, DateOnly created, Guid schemaId)
+        {
+            if (string.IsNullOrWhiteSpace(propName))
+                return Result<Prop>.Fail("Prop name must be not empty");
+            if (DateOnly.FromDateTime(DateTime.Now).CompareTo(created) < 0)
+                return Result<Prop>.Fail("Creation date must be not later than today");
+            if (schemaId.Equals(Guid.Empty))
+                return Result<Prop>.Fail("Schema Id must be not empty");
+
+            return Result<Prop>.Ok(new(propName, created, schemaId));
+        }
+
+        public Result UpdateName(string propName)
+        {
+            if (string.IsNullOrWhiteSpace(propName))
+                return Result.Fail("Prop name must be not empty");
+            PropName = propName;
+            return Result.Ok();
+        }
     }
 }
