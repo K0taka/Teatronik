e1b094a2412b35482b8edab7444e9292a7a7a3e3
K0taka <144228630+K0taka@users.noreply.github.com>
2025-05-07 05:50:22 +0500

    Type model in Core

--- Teatronik.Core/Models/Type.cs
+++ Teatronik.Core/Models/Type.cs
@@ -1,7 +1,33 @@
-﻿namespace Teatronik.Core.Models
+﻿using Teatronik.Core.Common;
+
+namespace Teatronik.Core.Models
 {
     public class Type
     {
+        public Guid Id { get; }
+        public string TypeName { get; private set; }
 
+        private Type(Guid id, string typeName)
+        {
+            Id = id;
+            TypeName = typeName;
+        }
+
+        public Type(string typeName) : this(Guid.NewGuid(), typeName) { }
+
+        public static Result<Type> Create(string typeName)
+        {
+            if (string.IsNullOrWhiteSpace(typeName))
+                return Result<Type>.Fail("Type name must be not empty");
+            return Result<Type>.Ok(new(typeName));
+        }
+
+        public Result UpdateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return Result.Fail("Type name must be not empty");
+            TypeName = name;
+            return Result.Ok();
+        }
     }
 }
